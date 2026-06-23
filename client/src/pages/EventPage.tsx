18db6fb0c9fc800251e5fe03768166ddb7b50b35
Marcelo Murilo <MM@MMs-MacBook-Pro.local>
2026-06-22 23:19:49 -0300

    feat: add Denys Nicolosi and Haroldo Lima testimonials (with video)

--- client/src/pages/EventPage.tsx
+++ client/src/pages/EventPage.tsx
@@ -111,6 +111,21 @@ const testimonials = [
     text: "Olá Murilo, ontem mesmo estava trocando mensagens com o Marcelo Martin (turma 1) e comentando como foi e tem sido muito proveitosa a mentoria com vocês!\n\nEstou conseguindo excelentes conexões, ainda não tive frutos dessas conexões mas me surpreendi com alguns comentários bem satisfatórios de CEOs e outros conselheiros e em números já adicionados mais de 600 contatos novos e importantes!\n\nSó tenho a agradecer e elogiar a mentoria, vale cada centavo investido!!!! E muito mais! Grande abraço e vamos nos falando!",
     highlightPhrase: "Só tenho a agradecer e elogiar a mentoria, vale cada centavo investido!",
   },
+  {
+    name: "Denys Nicolosi",
+    role: "Engenheiro, Professor e Empresário",
+    linkedin: "https://www.linkedin.com/in/denys-nicolosi-366b9a185/",
+    videoUrl: "https://marcelomurilo-my.sharepoint.com/:v:/g/personal/contato_marcelomurilo_com_br/IQD_X6IbZrsNSITDUrkblu7UAZRgGqqK-DkgyXBz21Daeow?e=0Jshjb",
+    text: "Diferente dos outros cursos de conselheiro que focam apenas na governança, essa mentoria foca em algo mais vital hoje, que é a construção de uma figura de autoridade. Ele entrega técnicas avançadas de inteligência artificial que potencializam muito a nossa especialidade, criando esse mecanismo de autoridade, principalmente no LinkedIn, de uma forma que eu nunca tinha visto.\n\nNa segunda parte, o Hamilton traz uma clareza impressionante entre entrega de valor e a precificação estratégica das nossas atividades. O que realmente me marcou muito foi o forte embasamento em conceitos éticos e também conceito missionário dessa nossa atuação.\n\nÉ uma mentoria inovadora e extremamente eficaz pra quem busca resultados práticos, pra você se posicionar com um nível de autoridade reconhecida e com excelência no que você já sabe fazer.",
+    highlightPhrase: "Ele entrega técnicas avançadas de inteligência artificial que potencializam muito a nossa especialidade, criando esse mecanismo de autoridade de uma forma que eu nunca tinha visto.",
+  },
+  {
+    name: "Haroldo Lima",
+    role: "Conselheiro",
+    videoUrl: "https://marcelomurilo-my.sharepoint.com/:v:/g/personal/contato_marcelomurilo_com_br/IQDbsUFKjzHZQ5AFGpnmYcvaAUR9u5sHeOdJxLhwVvt0RvU?e=6cC4oi",
+    text: "Eu confesso que no começo eu tava bastante reticente com relação a como a mentoria conseguiria me ajudar, até porque eu tava com cinco iniciativas, algumas bastante similares, outras não. E durante todas as sessões, eu vi que independentemente das minhas iniciativas, a mentoria conseguiu me ajudar, conseguiu me colocar num outro patamar.\n\nEu fiquei bastante, bastante mesmo impressionado, não só com a qualidade das discussões que nós tínhamos às segundas-feiras, mas principalmente com os materiais que você desenvolvia antes de cada sessão e principalmente aquele que você disponibilizava pós-sessões.\n\nRealmente era algo bastante tailor made, algo bastante focado em cada um dos mentorados. Isso realmente me ajudou e vem me ajudando bastante.",
+    highlightPhrase: "Com relação à mentoria, eu confesso que eu fiquei bastante, bastante mesmo impressionado, não só com a qualidade das discussões... mas principalmente com os materiais...",
+  },
 ];
 
 export default function EventPage() {
